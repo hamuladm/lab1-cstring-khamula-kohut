0136a6ff021217faa1af7583afa7a8a4542f13d8
hamula_dm <hamula.dm@gmail.com>
2023-09-23 13:08:35 +0300

    resolving conflicts

--- mystr.cpp
+++ mystr.cpp
@@ -22,7 +22,6 @@ my_str::my_str(size_t size, char initial): size_m(size) {
     // Copying
 my_str::my_str(const my_str& cstr){
     // Done: Kohut + Khamula;
-    size_m = cstr.size_m;
     capacity_m = size_m - (size_m % 16) + 16;
     data_m = new char[capacity_m + 1];
 
